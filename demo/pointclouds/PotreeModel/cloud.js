f17db64815087c2f173a9e0e8bcaa7e5e0d36299
saltyJeff <saltyJeff@users.noreply.github.com>
2019-03-18 14:14:58 -0700

    first without any history

---
+++ demo/pointclouds/PotreeModel/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2623038,
+    "boundingBox": {
+        "lx": -15.432000000000003,
+        "ly": -24.601,
+        "lz": -4.168,
+        "ux": 28.259,
+        "uy": 19.090000000000005,
+        "uz": 39.523
+    },
+    "tightBoundingBox": {
+        "lx": -15.432000000000003,
+        "ly": -24.601,
+        "lz": -4.168,
+        "ux": 18.808000000000008,
+        "uy": 19.090000000000005,
+        "uz": 2.7989999999999997
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.37837517261505129,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

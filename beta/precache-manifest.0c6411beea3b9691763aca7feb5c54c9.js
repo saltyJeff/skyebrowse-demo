201f3607cd6dde1299055e03ec0b413787b1c5ee
saltyJeff <saltyJeff@users.noreply.github.com>
2019-07-30 22:09:56 -0700

    added beta

---
+++ beta/precache-manifest.0c6411beea3b9691763aca7feb5c54c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53d125a23c61f2a68382923b74cee3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d9b60110c4c2a8b69c",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "557bc14c28081992db33",
+    "url": "/static/css/main.819d8ad7.chunk.css"
+  },
+  {
+    "revision": "07d9b60110c4c2a8b69c",
+    "url": "/static/js/2.94502d91.chunk.js"
+  },
+  {
+    "revision": "557bc14c28081992db33",
+    "url": "/static/js/main.f03bd448.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

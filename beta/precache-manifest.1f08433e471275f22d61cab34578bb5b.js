87683afc68bf9cfa06d1feb27a13bfed66ed0fab
saltyJeff <saltyJeff@users.noreply.github.com>
2019-07-30 22:21:05 -0700

    added basename offset

---
+++ beta/precache-manifest.1f08433e471275f22d61cab34578bb5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fbb79b4432e1b9e14f0f8bfb8da1fe",
+    "url": "/beta/index.html"
+  },
+  {
+    "revision": "7b05517aaad644f0de68",
+    "url": "/beta/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "2392a7abd33fbf0bda7d",
+    "url": "/beta/static/css/main.819d8ad7.chunk.css"
+  },
+  {
+    "revision": "7b05517aaad644f0de68",
+    "url": "/beta/static/js/2.ffe018b3.chunk.js"
+  },
+  {
+    "revision": "2392a7abd33fbf0bda7d",
+    "url": "/beta/static/js/main.78fd05ac.chunk.js"
+  },
+  {
+    "revision": "845ccd8c9a1d544499e0",
+    "url": "/beta/static/js/runtime~main.3484d553.js"
+  }
+]);
--- beta/service-worker.js
+++ beta/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c6411beea3b9691763aca7feb5c54c9.js"
+  "/beta/precache-manifest.1f08433e471275f22d61cab34578bb5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
